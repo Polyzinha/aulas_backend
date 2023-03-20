2a41e40e4473619f67fe7a7a07afb5c79f97cfea
Poliana Silveira de Oliveira <48172466854@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-20 08:39:43 -0300

    get

---
+++ aula12/exemplo_get.php
@@ -0,0 +1,7 @@
+<?php
+//Get é um comando que possibilita enviar e pegar dados com a url
+
+$resultado = $_GET['numero1'] + $_GET['numero2'];
+echo "O resultado da soma é $resultado";
+
+?>
---
+++ aula12/exercicio1.php
@@ -0,0 +1,6 @@
+<?php
+$tabuada = $_GET['tabuada'];
+for($i=0; $i<=10; $i++){
+    echo $_GET["tabuada"]*$i."<br>";
+    }
+?>
